3a76b957b7d474cc5ac4380d61f6e4ed31161c2a
Quin <quin.marilyn05@gmail.com>
2023-06-10 23:00:46 -0600

    Bootstrap the Swift rewrite.

---
+++ BatteryMon/BatteryMon/BatteryMonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryMonApp.swift
+//  BatteryMon
+//
+//  Created by Quin on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BatteryMonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatteryMon/BatteryMon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BatteryMon
+//
+//  Created by Quin on 6/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
